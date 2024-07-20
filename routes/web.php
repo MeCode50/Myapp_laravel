5a1f41c85b0c5ef46a1540038f4638ad395a5703
Munachimso <Victorycollins31@gmail.com>
2024-07-21 00:47:18 +0100

    created an array of jobs in routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['greeting' => 'hello']);
+    return view('home', ['jobs' => [
+        [
+            'id' => 1,
+            'title' => 'Frontend Developer',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'location' => 'New York, USA',
+            'created_at' => now()
+        ],
+         [
+            'id' => 2,
+            'title' => 'Backend Developer',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'location' => 'Texas',
+            'created_at' => now()
+        ]
+    ]]);
 });
 
 Route::get('/about', function () {
